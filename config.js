b7938a6d6b919b1854aa2a9b972f2afdd6ea2536
Krish-2008 <89650110+Krish-2008@users.noreply.github.com>
2022-03-19 15:01:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyAl3wQPlnCUUJWMacoXmAI5aNTiD1wOqo0",
+    authDomain: "project-71-53530.firebaseapp.com",
+    projectId: "project-71-53530",
+    storageBucket: "project-71-53530.appspot.com",
+    messagingSenderId: "821422572485",
+    appId: "1:821422572485:web:e83c7c7223a63d20dc81ac"
+  };
+
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
